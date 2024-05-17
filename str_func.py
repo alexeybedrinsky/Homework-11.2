eb7c5ac57d85f5f11a36a83d2fde361f0f27f5b6
Alexey Bedrinsky <bedrinsky@mail.ru>
2024-05-17 10:46:26 +0300

    Update str_func.py
    Добавлена функция с тремя заглавнымит буквами

--- str_func.py
+++ str_func.py
@@ -1,7 +1,16 @@
 def uppercase_letter(input_letter):
     return input_letter.upper()
+'''функция написания большими буквами'''
 
+def capitalize_first_letters(input_string):
+    """
+    Функция capitalize_first_letters принимает на вход строку и делает заглавными первые буквы каждого слова в ней.
 
-#input_letter = "напишем все большими буквами"
-#result = uppercase_letter(input_letter)
-#print(result)
+    Args:
+        input_string (str): Входная строка, которую необходимо преобразовать.
+
+    Returns:
+        str: Строка с заглавными первыми буквами каждого слова.
+    """
+    return ' '.join(word.capitalize() for word in input_string.split())
+
