438e1918c5f462908b4edb2fe56ff1b5cb85aad9
Alexey Bedrinsky <bedrinsky@mail.ru>
2024-05-17 10:27:50 +0300

    Докстринги добавлены
    Согласно запросу добавлены докстринги

--- str_func.py
+++ str_func.py
@@ -1,6 +1,6 @@
 def uppercase_letter(input_letter):
     return input_letter.upper()
-
+'''функция написания всех букв большими'''
 
 #input_letter = "напишем все большими буквами"
 #result = uppercase_letter(input_letter)
